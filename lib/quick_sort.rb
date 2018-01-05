ca22c73270150c09b1dfb5f150e03d5ed3e3c0f5
reid <reidsherman@gmail.com>
2018-01-04 20:44:22 -0800

    naive quicksort added

--- lib/quick_sort.rb
+++ lib/quick_sort.rb
@@ -1,15 +1,67 @@
+require 'byebug'
 class QuickSort
   # Quick sort has average case time complexity O(nlogn), but worst
   # case O(n**2).
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
+    return array if array.length < 2
+    pivot = array[0]
+    left = []
+    right = []
+    i = 1
+    while i < array.length 
+      if array[i] <= pivot
+        left << array[i]
+      else 
+        right << array[i]
+      end 
+      i += 1
+    end 
+    QuickSort.sort1(left) + [pivot] + QuickSort.sort1(right)
   end
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    # p array, length
+    if length <= 1
+      p start, length 
+      return array 
+    end 
+    # return array if length <= 1
+    pivot_idx = QuickSort.partition(array, start, length, &prc)
+    # QuickSort.sort2!(array, 0, pivot_idx) #sort the left 
+    QuickSort.sort2!(array, pivot_idx + 1, array.length - pivot_idx - 1)
+    # array 
   end
 
   def self.partition(array, start, length, &prc)
+    
+    barrier = start + 1 #keep track of sorted elements (less than pivot)
+    pivot = array[start]
+    length.times do |cnt|
+      current_idx = start + cnt
+      byebug if array[current_idx].nil?
+      if array[current_idx] < pivot
+        array[barrier], array[current_idx] = array[current_idx], array[barrier]
+        barrier += 1
+      end 
+    end 
+    array[start], array[barrier-1] = array[barrier-1], array[start]
+    barrier - 1 #return pivot index
   end
 end
+arr = [4,3,1,9,2,5,8,9]
+p arr
+QuickSort.sort2!(arr)
+p arr
+# arr = [4, 3, 2, 1, 7, 5, 8, 6]
+# pivot_idx = QuickSort.partition(arr, 4, 4)
+# p pivot_idx
+# p arr
+# arr = [3, 1, 5, 2, 4]
+
+# p QuickSort.partition(arr, 0, 5)
+
+# input = [5,4,6,3,2,2,5,1,10]
+# p QuickSort.sort1(input)
